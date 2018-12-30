bddbf639525211f4afede635ae5ecbe2437b602e
jkwebcoAI <jkwebcoai@gmail.com>
2018-12-30 18:20:52 -0500

    Update hash.py

--- image/hash.py
+++ image/hash.py
@@ -1,7 +1,7 @@
 import hashlib
  
 hasher = hashlib.md5()
-with open('sample-out.png', 'rb') as afile:
+with open('sample-out07891234.png', 'rb') as afile:
     buf = afile.read()
     hasher.update(buf)
 print(hasher.hexdigest())
