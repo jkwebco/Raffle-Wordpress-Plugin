e133ae1e3ad1349892570ff6ec89cbb2913cdb40
jkwebcoAI <jkwebcoai@gmail.com>
2019-01-01 14:02:02 -0500

    Create connector.py

---
+++ PuttingITTogether/connector.py
@@ -0,0 +1,9 @@
+ ```
+ from mysql.connector import (connection)
+
+cnx = connection.MySQLConnection(user='scott', password='password',
+                                 host='127.0.0.1',
+                                 database='employees')
+cnx.close()
+
+```
