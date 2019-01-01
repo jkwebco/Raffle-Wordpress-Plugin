71e3afee58921d6f92b2f7fdca9e9fb123c3d6ee
jkwebcoAI <jkwebcoai@gmail.com>
2019-01-01 14:11:39 -0500

    Update connector.php

--- PuttingITTogether/connector.php
+++ PuttingITTogether/connector.php
@@ -1,8 +1,10 @@
-<?php 
-global $wpdb;
-$results = $wpdb->get_results("SELECT * FROM student");
-foreach($results as $res)
-      echo $res->id;
-      echo $res->name;
-    }
-?>
+function simple_show_ticket_numbers_admin($order) {
+	global $wpdb;
+	$ticket_numbers = $wpdb->get_results('select * from '.$wpdb->prefix.'wooraffle_tickets_customer_to_tickets where order_id = "'.$order->get_id().'"');
+	if ($wpdb->num_rows > 0) {
+		echo '<p>&nbsp;</p><h2>Ticket Numbers          Raffle Hash         Raffle Number              Raffle Ticket</h2>';
+		foreach ( $ticket_numbers as $result ) {
+			echo $result->ticket_number.'/   '.$result->ticket_hash.'/   '.$result->ticket_numb.'/      '.$result->ticket_file.'<br />';
+		}
+	}
+}
